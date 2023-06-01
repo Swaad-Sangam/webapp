00d06b0e73e840418f21a88f66e49821c72a9fa5
Saksham Solanki <saksham.solanki@gmail.com>
2023-06-01 19:26:53 +0530

    Changes in API

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,10 +1,26 @@
 const dotenv = require('dotenv');
 
 dotenv.config()
+const API_KEY = process.env.SPOONACULAR_API_KEY
 
 function  getRecipe(query: string) {
-    const api_key = process.env.SPOONACULAR_API_KEY
-    const url = "https://api.spoonacular.com/recipes/complexSearch?query=" + query + "&apiKey=" + api_key
+    url = "https://api.spoonacular.com/recipes/complexSearch?query=" + query + "&apiKey=" + API_KEY
+    response = await fetch(url)
+    return response
+}
+
+function getRecipeByIngridents(ingredients: string[]){
+    url = "https://api.spoonacular.com/recipes/findByIngredients?ingredients="
+    ingredients.forEach(e=> {
+        url += "+" + e ","  
+    });
+    url += "&apiKey=" + API_KEY
+    response = await fetch(url)
+    return response
+}
+
+function getRecipeSteps(recipe_id : number) {
+    url = "https://api.spoonacular.com/recipes/" + recipe_id + "/analyzedInstructions" + "&apiKey=" + API_KEY
     response = await fetch(url)
     return response
 }
