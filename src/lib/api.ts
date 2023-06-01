75fe4e15f622bce34ef18df2f813d9498173e117
Saksham Solanki <saksham.solanki@gmail.com>
2023-06-01 19:45:52 +0530

    API Changes

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,26 +1,28 @@
+import { SPOONACULAR_API_KEY } from "$env/static/private";
+
 const dotenv = require('dotenv');
 
 dotenv.config()
-const API_KEY = process.env.SPOONACULAR_API_KEY
+const API_KEY = SPOONACULAR_API_KEY
 
-function  getRecipe(query: string) {
-    url = "https://api.spoonacular.com/recipes/complexSearch?query=" + query + "&apiKey=" + API_KEY
-    response = await fetch(url)
+async function  getRecipe(query: string) {
+    let url = "https://api.spoonacular.com/recipes/complexSearch?query=" + query + "&apiKey=" + API_KEY
+    let response = await fetch(url)
     return response
 }
 
-function getRecipeByIngridents(ingredients: string[]){
-    url = "https://api.spoonacular.com/recipes/findByIngredients?ingredients="
+async function getRecipeByIngridents(ingredients: string[]){
+    let url = "https://api.spoonacular.com/recipes/findByIngredients?ingredients="
     ingredients.forEach(e=> {
-        url += "+" + e ","  
+        url += "+" + e + ","  
     });
     url += "&apiKey=" + API_KEY
-    response = await fetch(url)
+    let response = await fetch(url)
     return response
 }
 
-function getRecipeSteps(recipe_id : number) {
-    url = "https://api.spoonacular.com/recipes/" + recipe_id + "/analyzedInstructions" + "&apiKey=" + API_KEY
-    response = await fetch(url)
+async function getRecipeSteps(recipe_id : number) {
+    let url = "https://api.spoonacular.com/recipes/" + recipe_id + "/analyzedInstructions" + "&apiKey=" + API_KEY
+    let response = await fetch(url)
     return response
 }
