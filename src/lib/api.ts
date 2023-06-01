400b14d52230500dd8ec9446badb5d8eec118c2d
Saksham Solanki <saksham.solanki@gmail.com>
2023-06-01 23:40:28 +0530

    API FIxes

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -8,7 +8,8 @@ const API_KEY = SPOONACULAR_API_KEY
 async function  getRecipe(query: string) {
     let url = "https://api.spoonacular.com/recipes/complexSearch?query=" + query + "&apiKey=" + API_KEY
     let response = await fetch(url)
-    return response
+    let data = await response.json()
+    return data
 }
 
 async function getRecipeByIngridents(ingredients: string[]){
@@ -18,11 +19,27 @@ async function getRecipeByIngridents(ingredients: string[]){
     });
     url += "&apiKey=" + API_KEY
     let response = await fetch(url)
-    return response
+    let data = await response.json()
+    return data
 }
 
 async function getRecipeSteps(recipe_id : number) {
     let url = "https://api.spoonacular.com/recipes/" + recipe_id + "/analyzedInstructions" + "&apiKey=" + API_KEY
     let response = await fetch(url)
-    return response
+    let data = await response.json()
+    return data
+}
+
+async function getRecipeId(query : string){
+    let url = "https://api.spoonacular.com/recipes/complexSearch?query=" + query + "&number=1&apiKey=" + API_KEY
+    let response = await fetch(url)
+    let data = await response.json()
+    return data.results[0].id
+}
+
+async function getNutirtionValue(recipe_id : string) {
+    let url = "https://api.spoonacular.com/recipes/" + recipe_id + "/nutritionWidget.json" + "&apiKey=" + API_KEY
+    let response = await fetch(url)
+    let data = await response.json()
+    return data
 }
