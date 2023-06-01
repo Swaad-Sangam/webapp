ba7dcc5a810fb4f904853f37d726d8b4f082951d
Vikramjeet Singh Dhaliwal <06917711621_ml@vips.edu>
2023-06-01 23:11:01 +0530

    little amendment

---
+++ src/routes/dashboard/profile/+page.svelte
@@ -0,0 +1,31 @@
+<script>
+	import { isLoading } from "$lib/stores";
+
+    isLoading.set(false);
+</script>
+
+<section class="left">
+    <section>
+        <div>Some content 1</div>
+        <div>Some more 1</div>
+    </section>
+    <section class="right">
+        <div>Some content 2</div>
+        <div>Some more 2</div>
+    </section>
+</section>  
+
+<style>
+    .left {
+        float: left;
+        width: 100%;
+    }
+    .left section {
+        width: 50%; 
+        float: left;
+    }
+    .right {
+        float: right;
+        width: 100%;
+    }
+</style>
