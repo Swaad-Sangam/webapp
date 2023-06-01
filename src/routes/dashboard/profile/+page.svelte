4f1aa192bff3824282935217155fe05194001a46
Vikramjeet Singh Dhaliwal <06917711621_ml@vips.edu>
2023-06-02 00:42:16 +0530

    Profile Started

--- src/routes/dashboard/profile/+page.svelte
+++ src/routes/dashboard/profile/+page.svelte
@@ -1,31 +1,44 @@
 <script>
-	import { isLoading } from "$lib/stores";
-
-    isLoading.set(false);
 </script>
 
-<section class="left">
-    <section>
-        <div>Some content 1</div>
-        <div>Some more 1</div>
-    </section>
-    <section class="right">
-        <div>Some content 2</div>
-        <div>Some more 2</div>
-    </section>
-</section>  
+<div class="container">
+    <img src="https://i.imgur.com/8Km9tLL.png" alt="user" /><br/>
+    <h2>Hello User</h2>
+    <p><strong>Email</strong>: user@universe.com</p>
+    <p><strong>User Name</strong> : </p>
+    <p><strong>Bio</strong>: </p> 
+</div>
 
 <style>
-    .left {
-        float: left;
-        width: 100%;
-    }
-    .left section {
-        width: 50%; 
-        float: left;
-    }
-    .right {
-        float: right;
-        width: 100%;
-    }
+body{
+    overflow: hidden;
+}
+.container{
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    width: 100vh;
+    margin: 25px auto;
+    text-align: center;
+    padding: 20px;
+    background: #fff;
+    border-radius: 15px;
+    box-shadow: 0 4px 10px 4px rgba(19, 35, 47, 0.3);
+    padding-left: 3rem;
+    padding-top: 2.5rem;
+}
+
+h2{
+    margin-top: 0;
+}
+p{
+    margin-bottom: 0;
+    margin-top: 0;
+}
+
+img{
+    border-radius: 10px;
+    height: 100px;
+    width: 100px;
+}
 </style>
